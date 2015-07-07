a3bd1d009785ddd6c3d708f724fd2e95feb9d4d9
Knut Inge Grøsland <hei@knutinge.com>
2015-07-08 00:26:30 +0200

    Removed unused code

--- COBezierTableViewDemo/COBezierTableViewDemo/AppDelegate.swift
+++ COBezierTableViewDemo/COBezierTableViewDemo/AppDelegate.swift
@@ -16,22 +16,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-    }
-
-
 }
 
